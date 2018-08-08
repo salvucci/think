3df94395b5cc36b53b6f093aba4bcd9cc4c5a7b8
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2018-08-08 14:31:13 -0400

    Moved text_to_words and count_syllables into its own utility module

--- think/modules/speech.py
+++ think/modules/speech.py
@@ -1,5 +1,6 @@
 import re
 from think import Module, Worker
+from .utilities import text_to_words, count_syllables
 
 
 class Speech(Module):
@@ -13,35 +14,10 @@ class Speech(Module):
 
     def add_say_fn(self, fn):
         self.say_fns.append(fn)
-
-    def _text_to_words(self, text):
-        text = str(text)
-        text = re.sub(r"[']", "", text)
-        text = re.sub(r"[^A-Za-z\s]", " ", text)
-        text = re.sub(r"\s+", " ", text)
-        return text.strip().split()
-
-    def count_syllables(self, text):
-        text = text.strip().lower()
-        count = 0
-        for word in text.split():
-            word = re.sub(r"^([^aeiouy]*)e$", "$1 ee", word)
-            word = re.sub(r"([^aeiouy])le$", "$1 ul", word)
-            word = re.sub(r"be$", " bee", word)
-            word = re.sub(r"es?$", "", word)
-            word = re.sub(r"ing$", " ing", word)
-            word = re.sub(r"([aeiouy][^aeiouy])ed$", "$1d", word)
-            word = re.sub(r"([^cs])ia", "$1i a", word)
-            word = re.sub(r"([^s])ea$", "$1e a", word)
-            word = re.sub(r"qu", "q", word)
-            word = re.sub(r"ienc", "i enc", word)
-            word = re.sub(r"ue", "u e", word)
-            word = re.sub(r"ual", "u al", word)
-            count += len(re.findall(r"[aeiouy]+", word))
-        return count
+        return self
 
     def calculate_duration(self, word):
-        return self.base_time + self.syllable_rate * self.count_syllables(word)
+        return self.base_time + self.syllable_rate * count_syllables(word)
 
     def _say_word(self, word, s1, s2, s3):
         self.worker.acquire()
@@ -55,10 +31,12 @@ class Speech(Module):
         self.worker.run(duration, s3 + ' "' + word + '"', fn)
 
     def say(self, text):
-        for word in self._text_to_words(text):
+        for word in text_to_words(text):
             self._say_word(word, "say", "saying", "said")
+        return self
 
     def subvocalize(self, text):
-        for word in self._text_to_words(text):
+        for word in text_to_words(text):
             self._say_word(word, "subvocalize",
                            "subvocalizing", "subvocalized")
+        return self
