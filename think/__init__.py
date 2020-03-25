e7fc6bf38f40f7128a3941a33b1a1bd0e21501eb
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-03-24 20:24:01 -0400

    Added speakers to inits

--- think/__init__.py
+++ think/__init__.py
@@ -1,8 +1,8 @@
 from .core import (Agent, Area, Buffer, Cancel, Clock, Data, Display, Item,
                    Keyboard, Location, Machine, Module, Mouse, Process, Query,
-                   Result, SlotQuery, Task, Values, Worker, World,
+                   Result, SlotQuery, Speakers, Task, Values, Worker, World,
                    get_think_logger)
 from .modules import (Audition, Aural, Chunk, Dwell, Eyes, EyeTracker,
-                      Fixation, Gaze, Motor, Instruction, Language, Memory,
+                      Fixation, Gaze, Instruction, Language, Memory, Motor,
                       Speech, Vision, Visual)
 from .skills import Arithmetic, Reading
--- think/core/__init__.py
+++ think/core/__init__.py
@@ -3,6 +3,6 @@ from .analysis import Data, Result, Values
 from .clock import Clock
 from .item import Area, Item, Location, Query, SlotQuery
 from .logger import get_think_logger
-from .machine import Display, Keyboard, Machine, Mouse
+from .machine import Display, Keyboard, Machine, Mouse, Speakers
 from .process import Cancel, Process
 from .world import Task, World
